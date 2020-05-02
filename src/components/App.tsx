d8afc45ef37e468c143bd8e8368493c6687d0b4e
Ilya Lukashevich <lukashevich.ilya000@gmail.com>
2020-05-02 07:39:41 +0300

    Added jss / edit git ignore config

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div>
-      I am alive, boyyy
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    I am alive, boyyy
+  </div>
+);
 
 export default App;
